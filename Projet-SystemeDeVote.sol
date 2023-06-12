39b124ab49ab85759acace24ebd5c189451605b0
sam-gvt <129540494+sam-gvt@users.noreply.github.com>
2023-06-13 00:01:50 +0200

    Update Projet-SystemeDeVote.sol

--- Projet-SystemeDeVote.sol
+++ Projet-SystemeDeVote.sol
@@ -1,9 +1,7 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
-pragma solidity ^0.8.20;
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 // remarque : le vote a egalite n'est pas pris en compte
 contract Voting is Ownable {
@@ -33,19 +31,30 @@ contract Voting is Ownable {
         VotesTallied
     }
 
-    mapping (address => Voter) whitelist;
+    mapping (address => Voter) whitelist ;
     Proposal[] proposalArray;
 
     // variable to follow the state of enum
-    WorkflowStatus stateWorkflowStatus;
+    WorkflowStatus public stateWorkflowStatus;
+
 
     modifier isWhiteList() {
         require(whitelist[msg.sender].isRegistered == true, "You are not authorized !");
         _;
     }
 
+    // ::::::::::::: GETTERS ::::::::::::: //
 
-    function nextWorkflowStatus() public external onlyOwner {
+    function getVoter(address _addr) external isWhiteList view returns (Voter memory) {
+        return whitelist[_addr];
+    }
+    
+    function getOneProposal(uint _id) external isWhiteList view returns (Proposal memory) {
+        return proposalArray[_id];
+    }
+
+    
+    function nextWorkflowStatus() external onlyOwner {
         require(uint8(stateWorkflowStatus) + 1 < 6, "End session !");
         uint8 nextState = uint8(stateWorkflowStatus) + 1;
 
@@ -54,24 +63,24 @@ contract Voting is Ownable {
     }
 
 
-    function RegisteringVoters(address _address) public external onlyOwner {
+    function RegisteringVoters(address _address) external onlyOwner {
         require(!whitelist[_address].isRegistered, "User already registered");
-        whitelist[address].isregistered=true;
+        whitelist[_address].isRegistered=true;
         emit VoterRegistered(_address);
     }
 
-    function ProposalsRegistration(string memory _proposal) public external isWhiteList {
+    function ProposalsRegistration(string memory _proposal) external isWhiteList {
         require(stateWorkflowStatus == WorkflowStatus.ProposalsRegistrationStarted, "Session is not active !");
         proposalArray.push(Proposal(_proposal, 0));
 
         emit ProposalRegistered(proposalArray.length - 1);
     }
 
-    function seeProposal() public external view isWhiteList returns(Proposal[] memory) {
+    function seeProposal() external view isWhiteList returns(Proposal[] memory) {
         return proposalArray;
     }
 
-    function VotingSession(uint _idProposal) public external isWhiteList {
+    function VotingSession(uint _idProposal) external isWhiteList {
         require(stateWorkflowStatus == WorkflowStatus.VotingSessionStarted, "Session is not active !");
         require(whitelist[msg.sender].hasVoted == false, "You can't vote 2 times !");
         
@@ -83,7 +92,7 @@ contract Voting is Ownable {
     }
 
 
-    function getWinner() public external view returns(uint) {
+    function getWinner() external view returns(uint) {
         require(stateWorkflowStatus == WorkflowStatus.VotesTallied, "Voting not yet closed !");
         uint winner;
         for (uint compteur=0; compteur < proposalArray.length; compteur ++) {
